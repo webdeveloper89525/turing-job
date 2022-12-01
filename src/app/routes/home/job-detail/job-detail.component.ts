9bb58d21f1faba677ab168babbc2c48b2fffd12e
enyingtan <webdeveloper89525@gmail.com>
2022-12-02 03:22:56 +0800

    fixed job detail

--- src/app/routes/home/job-detail/job-detail.component.ts
+++ src/app/routes/home/job-detail/job-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnChanges, OnInit, SimpleChanges, Input } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+
 import { IJob } from '../../../core/model/job.model';
 import { JobService } from '../../../core/service/job.service';
 
@@ -11,9 +13,13 @@ export class JobDetailComponent implements OnInit, OnChanges {
   @Input() selectedJob: IJob | null;
 
   job: IJob | null;
-  isLoading: boolean
+  isLoading: boolean;
+  description: SafeHtml | undefined;
 
-  constructor(public jobApi: JobService) {
+  constructor(
+    public jobApi: JobService,
+    private sanitizer: DomSanitizer,
+  ) {
     this.selectedJob = null;
     this.job = null;
     this.isLoading = false;
@@ -36,6 +42,7 @@ export class JobDetailComponent implements OnInit, OnChanges {
       this.isLoading = true;
       this.jobApi.getJob(jobId).subscribe((data: IJob) => {
         this.job = data;
+        this.description = this.job?.description ? this.sanitizer.bypassSecurityTrustHtml(this.job?.description) : undefined
         console.log('this.job: ', this.job);
         this.isLoading = false;
       });
